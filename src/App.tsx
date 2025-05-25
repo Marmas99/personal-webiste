511b7247b5fc8dcaacfd3435849d248b5c23305a
Markus Masalkovski <marmas99@gmail.com>
2025-05-25 20:51:51 +0200

    added some margin on the sides

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Card from "./components/Containers/Card/Card"
+import style from "./App.module.css"
 
 const App = () => {
   return (
     <>
-      <div>
+      <div className={style.App}>
         <Card />
       </div>
     </>
